0229ff843dd8aeba5fe987aa91703032a8b2da5c
Luessiaw <Luessiaw@pku.edu.cn>
2026-05-09 11:36:43 +0800

    增加 get_git_commit 以记录运行版本

--- examples/example_2.py
+++ examples/example_2.py
@@ -1,11 +1,19 @@
 from MEGssiaw import *
+from MEGssiaw.visualise import *
 
-'''固定位置的源和单个探头通道，计算测量值'''
+'''固定位置的源，在探头球面上产生的测量值分布，并绘制散点图'''
+
+# 源通道, 用于产生信号
+rp = np.array([0,0,0.08])
+rps = rp[None,:] # 只考虑一个方向
+nps = unit_y[None,:]
+# 源强度
+Q = np.array([10e-9,]) # 单位:Am
 
 # 探头通道。
-# 考虑探头位于头顶位置
+N = 10000
+# 1. 测量方向沿 x 轴
 rs = np.array([0,0,0.11])
-# 1. 矢量磁力仪, 三轴
 rss_v = np.array([rs,]*3)
 # 2. 标量磁力仪, 测量方向沿特定方向。
 rss_s = rs[None,:]
@@ -18,12 +26,6 @@ theta = np.deg2rad(30)
 unit = unit_x*np.cos(theta) + unit_y*np.sin(theta)
 nss_s = unit[None,:]
 
-# 源通道, 用于产生信号
-rp = np.array([0,0,0.08])
-rps = np.array([rp,rp]) # 两个通道，因为源有两个方向
-nps = np.array([unit_x,unit_y])
-# 源强度
-Q = np.array([0, 10e-9]) # 单位:Am
 
 # 导联场, 用于产生信号
 # 矢量磁力仪
@@ -35,7 +37,3 @@ L0_s = computeLeadFieldMatrix(rps,nps,rss_s,nss_s)
 B_v = L0_v @ Q
 B_s = L0_s @ Q
 
-print(B_v)
-print(B_s)
-
-print("Done")
